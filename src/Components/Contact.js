5f1c806f9dc4ae03eb08ad2b01bee45064faccea
Omkar-Gurav25 <onkargurav251@gmail.com>
2022-01-27 23:45:05 +0530

    contact page updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,19 +3,72 @@ import React from 'react'
 const Contact = () => {
     return (
         <>
-            <h1 className="text-center text-info">Contact Us</h1>
+            <div className='container country pt-5 bg-secondary'>
+                <h2 className='text-center py-4 text-light'>Greenford Overseas Around The World</h2>
+                <h5 className='px-5 text-light'>Presently the firm is Exporting to around 36 Countries like USA, UK, CANADA, OMAN, QATAR, JORDAN, SINGAPORE, KUWAIT, SAUDI ARABIA, ALGERIA, MOROCCO, TUNISIE, NIGERIA, etc and are eager to expand all over the GLOBE.</h5>
+
+
+                <div class="container table-data py-4">
+                    <table className='border'>
+                        <tbody className='border'>
+                            <tr>
+                                <td width="33%">
+                                    <ul style={{ listStyle: "square" }}>
+                                        <li>USA</li>
+                                        <li>UK</li>
+                                        <li>Canada</li>
+                                        <li>Oman</li>
+                                        <li>Jordan</li>
+                                        <li>Singhapore</li>
+                                        <li>UAE</li>
+                                        <li>Qatar</li>
+                                        <li>Oman</li>
+                                        <li>Egypt</li>
+                                        <li>Syria</li>
+                                        <li>Algeria</li>
+                                        <li>Iran</li>
+                                        <li>Iraq</li>
+                                    </ul>
+                                </td>
+                               
+                                <td width="33%">
+                                    <ul style={{ listStyle: "square" }}>
+                                        <li>Kuwait</li>
+                                        <li>Saudi Arabia</li>
+                                        <li>Algeria</li>
+                                        <li>Morocco</li>
+                                        <li>Tunisie</li>
+                                        <li>Nigeria</li>
+                                        <li>China</li>
+                                        <li>Singapore</li>
+                                        <li>Kuwait</li>
+                                        <li>Yemen</li>
+                                        <li>Pakistan</li>
+                                        <li>Saudi Arabia</li>
+                                        <li>Jordan</li>
+                                        <li>etc.</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+            <h1 className="text-center text-info pt-4 bg">Contact Us</h1>
             <div className="container">
                 <div class="mb-3  ">
                     <label for="exampleFormControlInput1" class="form-label">Name</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Name"/>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Name" />
                 </div>
                 <div class="mb-3  ">
                     <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Email id"/>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Email id" />
                 </div>
                 <div class="mb-3  ">
                     <label for="exampleFormControlInput1" class="form-label">Mobile No</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Mobile No"/>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Mobile No" />
                 </div>
                 <div class="mb-3  ">
                     <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
