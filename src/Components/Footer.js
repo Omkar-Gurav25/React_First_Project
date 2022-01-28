ee91e2c001a627dc28bcd70a3f618e330e836f7c
Omkar-Gurav25 <onkargurav251@gmail.com>
2022-01-28 23:27:47 +0530

    footer changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,9 +5,74 @@ const Footer = () => {
         <div class="card text-center bg-dark text-light mt-3">
 
             <div class="card-body my-3">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div className='container'>
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-6 col-md-4 d-flex align-items-stretch px-3 py-3">
+                            <div class="footer-sidebar-1">
+                                <section id="text-5" class="">
+                                    <h2 class="widget-title">Greenford Overseas</h2>
+                                    <div class="textwidget">
+
+                                        <p className='text-right'>--------OFFICE Address--------</p>
+                                        <p> Pune- 422 013, Maharashtra (India)</p>
+                                    </div>
+                                </section>
+                            </div>
+
+                        </div>
+                        <div className="col-xs-12 col-sm-6 col-md-4 d-flex align-items-stretch px-3 py-3">
+                            <div class="tg-footer-widget-area footer-sidebar-2">
+                                <section id="text-6" class="widget widget_text">
+                                    <h2 class="widget-title">Phone Number</h2>
+                                    <div class="textwidget"><p>+91- 8605928212</p>
+                                        <p><strong>Email: </strong>greenfordoverseas@gmail.com</p>
+                                    </div>
+                                </section>
+                            </div>
+
+                        </div>
+                        <div className="col-xs-12 col-sm-6 col-md-4 d-flex align-items-stretch px-3 py-3">
+                            {/* <div class="tg-footer-widget-area footer-sidebar-3">
+                                <section id="sfsi-widget-3" class="widget sfsi" style="min-height: 79px;">
+                                    <h2 class="widget-title">Follows Us</h2>
+                                    <div class="sfsi_widget" data-position="widget" style="display:flex;flex-wrap:wrap;justify-content: left">
+                                        <div id="sfsi_wDiv"></div>
+                                        <div class="norm_row sfsi_wDiv " style="width:225px;position:absolute;;text-align:left">
+                                            <div style="width: 40px; height: 40px; margin-left: 5px; margin-bottom: 5px; z-index: 1000000;" class="sfsi_wicons shuffeldiv ">
+                                                <div class="inerCnt" style="z-index: 999;">
+                                                    <a class=" sficn" data-effect="fade_in" target="_blank" href="https://www.facebook.com/Greenford-Overseas-109273547930356/" id="sfsiid_facebook_icon" style="opacity: 0.6;">
+                                                        <img data-pin-nopin="true" alt="Facebook" title="Facebook" src="https://www.greenfordoverseas.com/wp-content/plugins/ultimate-social-media-icons/images/icons_theme/default/default_facebook.png" width="40" height="40" style="" class="sfcm sfsi_wicon " data-effect="fade_in" />
+                                                    </a>
+                                                </div>
+                                            </div>
+                                            <div style="width: 40px; height: 40px; margin-left: 5px; margin-bottom: 5px; z-index: 0;" class="sfsi_wicons shuffeldiv ">
+                                                <div class="inerCnt" style="z-index: 999;">
+                                                    <a class=" sficn" data-effect="fade_in" target="_blank" href="https://instagram.com/greenfordoverseas" id="sfsiid_instagram_icon" style="opacity: 0.6;">
+                                                        <img data-pin-nopin="true" alt="Instagram" title="Instagram" src="https://www.greenfordoverseas.com/wp-content/plugins/ultimate-social-media-icons/images/icons_theme/default/default_instagram.png" width="40" height="40" style="" class="sfcm sfsi_wicon " data-effect="fade_in" />
+                                                    </a>
+                                                </div>
+                                            </div>
+                                            <div style="width: 40px; height: 40px; margin-left: 5px; margin-bottom: 5px; z-index: 0;" class="sfsi_wicons shuffeldiv ">
+                                                <div class="inerCnt" style="z-index: 999;">
+                                                    <a class=" sficn" data-effect="fade_in" target="_blank" href="http://linkedin.com/in/greenford-overseas-b1836420b" id="sfsiid_linkedin_icon" style="opacity: 0.6;">
+                                                        <img data-pin-nopin="true" alt="Linkedln" title="Linkedln" src="https://www.greenfordoverseas.com/wp-content/plugins/ultimate-social-media-icons/images/icons_theme/default/default_linkedin.png" width="40" height="40" style="" class="sfcm sfsi_wicon " data-effect="fade_in" />
+                                                    </a>
+                                                </div>
+                                            </div>
+                                            <div style="width: 40px; height: 40px; margin-left: 5px; margin-bottom: 5px; z-index: 0;" class="sfsi_wicons shuffeldiv ">
+                                                <div class="inerCnt">
+                                                    <a class=" sficn" data-effect="fade_in" target="_blank" href="https://youtube.com/channel/UCxbK4mAN2-mmu_t2czPr8ZQ" id="sfsiid_youtube_icon" style="opacity:0.6">
+                                                        <img data-pin-nopin="true" alt="Youtube" title="Youtube" src="https://www.greenfordoverseas.com/wp-content/plugins/ultimate-social-media-icons/images/icons_theme/default/default_youtube.png" width="40" height="40" style="" class="sfcm sfsi_wicon " data-effect="fade_in" />
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+                            </div> */}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
